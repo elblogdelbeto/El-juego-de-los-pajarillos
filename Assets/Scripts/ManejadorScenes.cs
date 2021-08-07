e63f5a76a55ac37d18b7e887c44823181ffd4454
Alberto Najera Dominguez <elblogdelbeto@outlook.com>
2021-08-07 01:58:27 -0500

    antes de actualizar version

--- Assets/Scripts/ManejadorScenes.cs
+++ Assets/Scripts/ManejadorScenes.cs
@@ -1,36 +1,44 @@
 ﻿/* Para moverte entre scenes y darle un tiempo de espera antes de cargar 
  */
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ManejadorScenes : MonoBehaviour
 {
-
     public float tiempoCargaAutNivel = 0;
-
 
     private void Start()
     {
         if (tiempoCargaAutNivel > 0f)
+        {
             Invoke("CargarSigEscena", tiempoCargaAutNivel);
+        }
     }
 
+    /// <summary>
+    /// Carga la SCENE siguiente al actual en base al index
+    /// </summary>
     public void CargarSigEscena()
     {
         string name = SceneManager.GetActiveScene().name;
         if (!string.IsNullOrEmpty(name))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //anteriormente: Application.LoadLevel(name);
-            Debug.Log("Carga del nuevo nivel: " + name);
+            int indiceSceneSiguiente = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(indiceSceneSiguiente); //anteriormente: Application.LoadLevel(nme);
+            Debug.Log("Carga de la siguiente Scene: " + SceneManager.GetSceneAt(indiceSceneSiguiente).name);
         }
     }
 
+    /// <summary>
+    /// Carga una Scene en base a su nombre
+    /// </summary>
+    /// <param name="name">nombre de la Scene a cargar</param>
     public void CargarEscena(string name)
     {
         Debug.Log("Carga del nuevo nivel: " + name);
         SceneManager.LoadScene(name); //anteriormente: Application.LoadLevel(name);
     }
+
 
     public void QuitarJuego()
     {
